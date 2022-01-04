ade048049d4d9b48f0d33a5dad0620b16ec93c4f
gbxander <gbxander14@gmail.com>
2022-01-03 21:56:02 -0800

    create server with routes and rendering logic

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+
+const app = express()
+
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.listen(port, (req, res) => {
+    console.log("listening on port: " + port)
+})
+
+app.use(logger('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home', {title: 'Home'})
+})
+
+app.get('/sign-up', (req, res) => {
+    res.render('sign-up', {title: 'Sign-up'})
+})
+
+app.get('/sign-in', (req, res) => {
+    res.render('sign-in', {title: "Sign-in"})
+})
+
+app.get('/posts', (req, res) => {
+    const posts = []
+    res.render('posts', {title: "All Posts", posts})
+})
+
+app.get('/my-posts', (req, res) => {
+    const posts = []
+    res.render('posts', {title: "My Posts", posts})
+})
+
+app.use((req, res) => {
+    res.status(404).render('404', {title: '404'})
+})
