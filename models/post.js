3e032194ba4ae073cab92d8b828ed2e0de509b70
gbxander <gbxander14@gmail.com>
2022-01-05 22:34:57 -0800

    server: update author field for post schema

--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ const postSchema =  new Schema({
             type: String,
             required: true
         },
-        name: {
+        username: {
             type: String,
             required: true
         }
